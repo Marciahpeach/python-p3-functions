cd332b91e25c55a48db6f96caee16280598d8d96
Marciah <marciah.machoka@student.moringaschool.com>
2025-05-08 12:52:21 +0300

    complete py

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
-def greet_programmer():
-    pass
+
+def greet_programmer(name="programmer"):
+    print(f"Hello, {name}!")
+greet_programmer()
 
 def greet(name):
-    pass
-
-def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+greet("Naureen")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+print(add(1, 2))
 
 def halve(number):
-    pass
+    return number / 2
+print(halve(2))
+
+def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
+greet_with_default()
